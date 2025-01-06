0c98e63284d65a7825c1da85de0b6bef22e8a721
hitohata <hirohatatro@gmail.com>
2025-01-06 15:36:18 -0800

    add build settings

--- private-docusaurus/next.config.ts
+++ private-docusaurus/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: "/saurus/:path*", destination: "/saurus/:path*/index.html"
+    }
+  ]
 };
 
 export default nextConfig;
